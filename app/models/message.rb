cbefc4f7f94c4e033927ff3f726eaa69e436de45
saki <manicon22@gmail.com>
2016-06-14 22:12:08 +0000

    年齢カラムの表示とバリデートの追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ActiveRecord::Base
     validates :name,  length: {maximum: 20} , presence: true 
     validates :body,  length: {maximum: 10, minimum: 2} , presence: true 
+    validates :age,  length: {minimum: 0} , presence: true ,numericality: { only_integer: true }
 end
